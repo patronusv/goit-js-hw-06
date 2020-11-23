c385dccb94d7ec060959253cd0c78ae628ea20e2
Viktor Gorokhov <patronusv@gmail.com>
2020-11-23 14:53:13 +0200

    добавляет решение 3-го задания

--- js/tasks.js
+++ js/tasks.js
@@ -12,3 +12,8 @@ const getUsersWithEyeColor = (users, color) => {
   return users.filter(({ eyeColor }) => eyeColor === color);
 };
 console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+const getUsersWithGender = (users, gender) => {
+  return users.filter(({ gender: userGender }) => userGender === gender).map(({ name }) => name);
+};
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
