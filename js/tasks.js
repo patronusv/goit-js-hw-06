c86a69d8faf51a96fd1ccde466b90711d0aaf9d5
Viktor Gorokhov <patronusv@gmail.com>
2020-11-23 17:07:37 +0200

    добавляет решение 10-го задания

--- js/tasks.js
+++ js/tasks.js
@@ -53,3 +53,12 @@ const getNamesSortedByFriendsCount = users => {
 };
 console.log(getNamesSortedByFriendsCount(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
+
+const getSortedUniqueSkills = users => {
+  return users
+    .reduce((allSkills, user) => [...allSkills, ...user.skills], [])
+    .filter((value, index, self) => self.indexOf(value) === index)
+    .sort();
+};
+console.log(getSortedUniqueSkills(users));
+// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
