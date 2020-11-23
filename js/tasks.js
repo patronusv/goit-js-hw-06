48bf8de59451c2dd41b6e13bb239c00dc15fe4ab
Viktor Gorokhov <patronusv@gmail.com>
2020-11-23 14:48:29 +0200

    добавляет решение первого задания

--- js/tasks.js
+++ js/tasks.js
@@ -1,3 +1,9 @@
 'use strict';
 import { default as users } from './users.js';
 console.log(users);
+
+const getUserNames = users => {
+  return users.map(user => user.name);
+};
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
