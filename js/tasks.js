a7d705390a217c53e6594897339f647aadf2ba32
Viktor Gorokhov <patronusv@gmail.com>
2020-11-23 15:16:01 +0200

    добавляет решение 6-го задания

--- js/tasks.js
+++ js/tasks.js
@@ -29,3 +29,12 @@ const getUserWithEmail = (users, email) => {
 };
 console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
 console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+
+const getUsersWithAge = (users, min, max) => {
+  return users.filter(({ age }) => age >= min).filter(({ age }) => age <= max);
+};
+
+console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
+
+console.log(getUsersWithAge(users, 30, 40));
+// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
